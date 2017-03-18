a87b5e2a027e0fc7fe510024313509010656115a
Loduis Madariaga <loduis@gmail.com>
2017-03-18 07:03:22 -0500

    fix test schemas.

--- src/Support/Currency.php
+++ src/Support/Currency.php
@@ -4,6 +4,8 @@ namespace Alegra\Support;
 
 class Currency extends \Illuminate\Api\Resource\Model
 {
+    protected static $visible = ['*'];
+
     protected $fillable = [
         'code'   => 'string',
         'symbol' => 'string'
